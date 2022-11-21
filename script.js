aaddedaf2449957ca5e188e0aa2f2070ec9d1127
pedram <pedramvahdati.r@gmail.com>
2022-11-21 09:50:18 +0330

    background object added

--- script.js
+++ script.js
@@ -6,12 +6,27 @@ var frames = 0;
 var sprite = new Image();
 sprite.src = "./images/sprite.png";
 
-   
+var bg = {
+    sX : 0, 
+    sY : 0, 
+    w : 224, 
+    h : 400, 
+    x : 0, 
+    y : cvs.height - 400, 
+    darw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w
+            , this.h)
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.w, this.y, this.w
+            , this.h)
+    }
+}
 
 function update(){
 
 }
 function darw(){
+    ctx.fillStyle = "#4ec0ca";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
 
 
 }
